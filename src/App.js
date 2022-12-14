7cf506ab0bf49e4662b132ab4588502d62c0472f
cathciyal <aruna.cathciyal24@gmail.com>
2022-12-14 19:25:45 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import "./App.css";
+
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Note from "./components/Note";
+
+function App(props) {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((prevValue) => {
+      return [...prevValue, newNote];
+    });
+  }
+
+  function deleteNotes(id) {
+    setNotes((preValue) => {
+      return [...preValue.filter((note, index) => index !== id)];
+    });
+  }
+  return (
+    <div className="App">
+      <Header />
+      <Main onAdd={addNote} />
+      {notes.map((note, index) => (
+        <Note
+          key={index}
+          id={index}
+          title={note.title}
+          content={note.content}
+          onDelete={deleteNotes}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header() {
+ 
+  
+  return (
+    <div style={{backgroundColor:"#FFBF00"}}className="header">
+      <h1 style={{color:"white"}}>Keeper</h1>
+    </div>
+  );
+}
+
+export default Header;
